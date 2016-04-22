1c406aedb993d04d5e0407ddd95c1a066a2d04c1
Ilya Breev <ilyabreev@gmail.com>
2016-04-22 15:36:08 +0300

    Adhoc query plans in plan cache.

---
+++ Memory/AdHocQueryPlansInPlanCache.sql
@@ -0,0 +1,12 @@
+/*****************************************
+Скомпилированные на лету запросы, которые находятся в кеше планов
+*******************************************/
+SELECT cp.objtype AS ObjectType,
+OBJECT_NAME(st.objectid,st.dbid) AS ObjectName,
+cp.usecounts AS ExecutionCount,
+st.TEXT AS QueryText,
+qp.query_plan AS QueryPlan
+FROM sys.dm_exec_cached_plans AS cp
+CROSS APPLY sys.dm_exec_query_plan(cp.plan_handle) AS qp
+CROSS APPLY sys.dm_exec_sql_text(cp.plan_handle) AS st
+WHERE cp.objtype = 'AdHoc'
